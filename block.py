fd3603aa6ba09aa362edb6b54450a4362ac5c377
diggeridoo95 <eemilxhakala@gmail.com>
2026-02-11 12:35:05 +0200

    Bugikorjaus block.py: undo_rotationissa

--- block.py
+++ block.py
@@ -30,7 +30,7 @@ class Block:
 
 	def undo_rotation(self):
 		self.rotation_state -= 1
-		if self.rotation_state == 0:
+		if self.rotation_state < 0:
 			self.rotation_state = len(self.cells) - 1
 	
 	def draw(self, screen, offset_x, offset_y):
@@ -39,12 +39,4 @@ class Block:
 			tile_rect = pygame.Rect(offset_x + tile.column * self.cell_size, 
 				offset_y + tile.row * self.cell_size, self.cell_size -1, self.cell_size -1)
 			pygame.draw.rect(screen, self.colors[self.id], tile_rect)
-
-	def get_cell_positions(self):
-		tiles = self.cells[self.rotation_state]
-		moved_tiles = []
-		for position in tiles:
-			position = Position(position.row + self.row_offset, position.column + self.column_offset)
-			moved_tiles.append(position)
-		return moved_tiles
 		
