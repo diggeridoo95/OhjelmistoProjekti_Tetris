eb7f87fe035a975a7c8b1ea1b894eead3214e525
JukkaVilpponen <jukka.vilpponen@seamk.fi>
2026-02-12 17:59:16 +0200

    Jatkuvaliike

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ clock = pygame.time.Clock()
 
 game = Game()
 
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -24,6 +27,8 @@ while True:
 				game.move_down()
 			if event.key == pygame.K_UP:
 				game.rotate()
+		if event.type == GAME_UPDATE:
+			game.move_down()
 
 	screen.fill(Colors.dark_blue)
 	game.draw(screen)
